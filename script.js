a37bafb053ae8f9788cf490835c5ce95075d1240
dakota <kotalilyy@gmail.com>
2021-02-18 14:18:22 -0600

    add variables for keys

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const WHITE_KEYS = ['z', 'x', 'c', 'v', 'b', 'n', 'm']
+const BLACK_KEYS = ['s', 'd', 'g', 'h', 'j']
+
 const keys = document.querySelectorAll('.key')
 
 keys.forEach(key => {
@@ -6,5 +9,11 @@ keys.forEach(key => {
 
 function playNote(key) {
     const noteAudio = document.getElementById(key.dataset.note)
+    noteAudio.currentTime = 0
     noteAudio.play()
+    key.classList.add('active')
+    noteAudio.addEventListener('ended', () => {
+    key.classList.remove('active')
+    })
 }
+
